a3f3cbb7ad606b10e985a3435483587a72dae26a
Tom Sydney Kerckhove <syd@fpcomplete.com>
2017-10-11 05:36:53 +0200

    New emacs config

---
+++ shared/shared/spacemacs/private-org/config.el
@@ -0,0 +1,41 @@
+(setq org-bullets-bullet-list '("*" "**" "***" "****"))
+; Turn off org-indent-mode
+(with-eval-after-load 'org (setq org-startup-indented nil))
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "WAITING(w)" "READY(r)" "|" "DONE(d)" "CANCELLED(c)")))
+(setq org-todo-keyword-faces
+      '(("TODO" . "red")
+        ("NEXT" . "orange")
+        ("STARTED" . "orange")
+        ("WAITING" . "blue")
+        ("READY" . "brown")
+        ("DONE" . "green")
+        ("CANCELLED" . "green")))
+
+(setq org-directory "~/workflow/")
+(setq org-default-notes-file (concat org-directory "/inbox.txt"))
+(setq org-agenda-custom-commands
+       '(
+         ("f" "Work actions"
+          (
+           (tags-todo "work/STARTED|NEXT|READY|WAITING")
+           (agenda "")
+           )
+         )
+         ("w" "WAITING actions"
+          (
+           (todo "WAITING")
+           (agenda "")
+           )
+         )
+         ("n" "All next actions and agenda"
+          (
+           (todo "STARTED|NEXT")
+           (agenda "")
+          )
+         )
+        )
+   )
+
+(setq org-agenda-files '("~/workflow" "~/workflow/projects"))
